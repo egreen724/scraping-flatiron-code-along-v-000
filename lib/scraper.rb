5f6486c3887d7612e4ab9dc3f101df9a740ad779
Erin Green <erin.y.green@gmail.com>
2018-10-17 17:34:23 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,8 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
-    
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry 
 end
   
   
@@ -25,4 +26,4 @@ end
 end
 
 
-
+Scraper.new.get_page
