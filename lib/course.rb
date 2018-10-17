74d3af8679c426b7dabea746d25e6688934e0aad
Erin Green <erin.y.green@gmail.com>
2018-10-17 17:26:03 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,4 +1,6 @@
 #The Scraper class will be responsible for using Nokogiri to scrape the relevant data. It will also be responsible for taking that data and using it to instantiate instances of the Course class.
 
-
+class Course
+  
+end
 
